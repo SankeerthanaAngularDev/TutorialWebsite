480bbea6b6c7d0a73d7a4fe22ef64c44e98b00c1
SankeerthanaAngularDev <lakshmi.sankeerthana@gmail.com>
2018-09-11 18:02:23 +0530

    inserted image slider in home page

--- js/app.js
+++ js/app.js
@@ -1,10 +1,52 @@
-var app = angular.module("myApp", ["ngRoute"]);
+var app = angular.module("myApp", ["ngRoute", "ui.bootstrap"]);
+app.controller("LoginCntrl", function ($scope) {
+
+});
+
+
+
+// Controller  for Carousel
+app.controller("CarouselCtrl", function ($scope) {
+
+    // initializing the time Interval
+    $scope.myInterval = 5000;
+
+    // Initializing  slide rray
+    $scope.slides = [
+        {
+            image: 'assets/black-background-design.jpg',
+            text: 'Image1'
+        },
+
+        {
+            image: 'assets/image.jpg',
+            text: 'Image2'
+        },
+        {
+            image: 'assets/rabbit.jpg',
+            text: 'Image3'
+        },
+        {
+            image: 'assets/family-bear-figures-funny-160460.jpeg',
+            text: 'Image4'
+        }
+                      ];
+
+    // var slides = $scope.slides;
+    //console.log(slides);
+
+
+
+
+}); // Controller Ends here
+
+
 
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/SignIn", {
             templateUrl: "views/login.html",
-            controller: ""
+            controller: "LoginCntrl"
         })
         .when("/SignUp", {
             templateUrl: "views/signup.html",
@@ -12,6 +54,6 @@ app.config(function ($routeProvider) {
         })
         .when("/Home", {
             templateUrl: "views/main.html",
-            controller: ""
+            controller: "CarouselCtrl"
         });
 });
