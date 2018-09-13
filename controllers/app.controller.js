dd63049802a26d0c21d1c2b2b8fe23080069b43a
SankeerthanaAngularDev <lakshmi.sankeerthana@gmail.com>
2018-09-13 14:58:46 +0530

    shifted controller to controllers file from app.js file

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,1 +1,40 @@
+app.controller("LoginCntrl", function ($scope) {
 
+});
+
+
+
+// Controller  for Carousel
+app.controller("CarouselCtrl", function ($scope) {
+
+    // initializing the time Interval
+    $scope.myInterval = 3000;
+
+    // Initializing  slide rray
+    $scope.slides = [
+        {
+            image: 'assets/black-background-design.jpg',
+            text: 'Image1'
+        },
+
+        {
+            image: 'assets/image.jpg',
+            text: 'Image2'
+        },
+        {
+            image: 'assets/rabbit.jpg',
+            text: 'Image3'
+        },
+        {
+            image: 'assets/family-bear-figures-funny-160460.jpeg',
+            text: 'Image4'
+        }
+                      ];
+
+    // var slides = $scope.slides;
+    //console.log(slides);
+
+
+
+
+}); // Controller Ends here
